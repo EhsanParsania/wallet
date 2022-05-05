cecf1b34015fc89fd924ecb36f453330a50398e3
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2022-05-05 11:30:30 +0300

    add reset wallet amount

--- contracts/Wallet/Wallet.sol
+++ contracts/Wallet/Wallet.sol
@@ -4,23 +4,30 @@ pragma solidity ^0.8.0;
 
 import "./Ownable.sol";
 
-contract Wallet is Ownable  {
-  
+contract Wallet is Ownable {
     uint256 internal savedAmount = 0;
 
     //write method
-    function addCoin(uint256 _amount) external  returns(uint256  , uint256 ) {
-       savedAmount += _amount ;
-       return ( _amount ,  savedAmount);
+    function addCoin(uint256 _amount) external returns (uint256, uint256) {
+        savedAmount += _amount;
+        return (_amount, savedAmount);
+    }
+
+    //write method
+    function resetWallet() external {
+        savedAmount = 0;
     }
 
     //read method
-    function getCoinAmount() external view returns(uint256 ){
-     return savedAmount;
+    function getCoinAmount() external view returns (uint256) {
+        return savedAmount;
     }
 
-      modifier theOwner {
-        require( _msgSender() == owner() || owner() == tx.origin , "To call this method you have to be owner or subAdmin!");
-         _;
+    modifier theOwner() {
+        require(
+            _msgSender() == owner() || owner() == tx.origin,
+            "To call this method you have to be owner or subAdmin!"
+        );
+        _;
     }
 }
