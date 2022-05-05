13b81b5cfc07ba3abdca0062879db7e7f6a14ef2
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2022-05-05 11:15:58 +0300

    create transfer ownership

--- contracts/Wallet/Ownable.sol
+++ contracts/Wallet/Ownable.sol
@@ -22,7 +22,16 @@ abstract contract Ownable is Context {
 
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor() {
+        _transferOwnership(_msgSender());
+    }
 
+    /**
+     * @dev Returns the address of the current owner.
+     */
     function owner() public view virtual returns (address) {
         return _owner;
     }
@@ -43,5 +52,22 @@ abstract contract Ownable is Context {
      * thereby removing any functionality that is only available to the owner.
      */
 
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _transferOwnership(newOwner);
+    }
 
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Internal function without access restriction.
+     */
+    function _transferOwnership(address newOwner) internal virtual {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
 }
